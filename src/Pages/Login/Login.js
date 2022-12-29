fe62723f8cc5639633114fc90f07ba2b12d16b97
kayesh01 <shaanfarhan01@gmail.com>
2022-12-29 20:32:26 +0600

    first commit

---
+++ src/Pages/Login/Login.js
@@ -0,0 +1,35 @@
+import React, { useState } from 'react';
+import { useForm } from 'react-hook-form';
+import { Link } from 'react-router-dom';
+
+const Login = () => {
+    const { register, handleSubmit } = useForm();
+
+    const handleLogin = data => {
+        console.log(data);
+    }
+
+    return (
+        <div className='h-[800px] flex justify-center items-center'>
+            <div className='w-96 p-7'>
+                <h2 class="mb-6 text-3xl">This is Login.</h2>
+                <form onSubmit={handleSubmit(handleLogin)}>
+                    <div class="mb-6">
+                        <label for="email" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Email address</label>
+                        <input {...register("email")} type="email" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                    <div class="mb-6">
+                        <label for="passwrod" class="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Password</label>
+                        <input {...register("password")} type="password" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" required />
+                    </div>
+                    <button type="button submit" class="w-full text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-blue-600 dark:hover:bg-blue-700 focus:outline-none dark:focus:ring-blue-800">LogIn</button>
+                    {/* <input className='btn btn-accent' value="LogIn" type="submit" /> */}
+                </form>
+                <p class='my-3'>New to Task Manager?<Link className='text-purple-600' to='/signup'>Create an account</Link></p>
+                <button type="button" class="focus:outline-none w-full text-white bg-green-700 hover:bg-green-800 focus:ring-4 focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800">GOOGLE LOG IN</button>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
